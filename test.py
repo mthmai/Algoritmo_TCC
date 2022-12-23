fce4c159a1500e49f6091f87b43dc75001230729
mthmai <matheuspereiramai@gmail.com>
2022-12-23 10:19:04 -0300

    adicionei a tabuada

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
 print('Esse é um teste')
 
+n =  int ( input ( print ( 'Insira um número:' )))
+
+for  i in  range ( 0 , 11 ):
+	print ( f' { i } x { n } = { i * n } ' )
+print ( f'Essa e a tabuada do { n } ' )
